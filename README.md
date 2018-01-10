b29e9d262284059f50326523814b005a18a8fbd2
John Wang <johncwang@gmail.com>
2018-01-09 21:36:05 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# RingCentral PermaHooks
+
+[![Build Status][build-status-svg]][build-status-link]
+[![Go Report Card][goreport-svg]][goreport-link]
+[![Docs][docs-godoc-svg]][docs-godoc-link]
+[![License][license-svg]][license-link]
+
+This is a small app that turns RingCentral's expiring outbound webhooks into non-expiring webhooks. This is especially useful when connecting to a service with a simple inbound webhook implementation such as Zapier. This is useful because RingCentral's webhook implementation has a couple of security features that are not supported by all webhook consuming services:
+
+* RingCentral requires the webhook endpoint return the request `Validation-Token` header in the response.
+* RingCentral webhooks expire to ensure the correct site is receiving the information.
+
+However, some sites can only receive a simple webhook post. This service an convert RingCentral's secure webhooks into a simple webhook for these services.
+
+This service does a few things:
+
+* Creates a RingCentral webhook subscription
+* Renews the RingCentral webhook subscription
+* Handles the `Validation-Token` response
+
+See more information about creating RingCentral webhooks here:
+
+* API Reference: https://developer.ringcentral.com/api-docs/latest/index.html#!#RefCreateSubscription
+* Developer Guide: http://ringcentral-api-docs.readthedocs.io/en/latest/webhooks/
+
+## Installation and Configuration
+
+Before you can complete the following installation procedure, you need to get a webhook URL from your downstream service. For example, a Zapier webhook URL.
+
+```bash
+$ go get github.com/grokify/ringcentral-permahooks
+$ cd $GOPATH/src/github.com/grokify/ringcentral-permahooks
+$ cp sample.env .env
+$ vi .env
+$ go run server.go
+```
+
+### Tunneling
+
+If your server is behind a NAT or a firewall and not accessible via the Internet, you can use a tunneling service such as [ngrok](https://ngrok.com/). In the following example, you would create a RingCentral webhook to `https://12345678.ngrok.io/webhook` which you would set as `PERMAHOOKS_INBOUND_WEBHOOK_URL` in your environment.
+
+```bash
+$ ngrok http 8080
+
+ngrok by @inconshreveable                                                                             (Ctrl+C to quit)
+                                                                                                                      
+Tunnel Status                 online                                         
+Update                        update available (version 2.2.8, Ctrl-U to update)
+Version                       2.0.25/prod                              
+Region                        United States (us)                            
+Web Interface                 http://127.0.0.1:4040                            
+Forwarding                    http://12345678.ngrok.io -> localhost:8080       
+Forwarding                    https://12345678.ngrok.io -> localhost:8080                                             
+                     
+Connections                   ttl     opn     rt1     rt5     p50     p90
+                              83      0       0.00    0.00    18.68   301.08
+```
+
+## Support
+
+If you have questions or support, please use the following resources:
+
+ [build-status-svg]: https://api.travis-ci.org/grokify/ringcentral-permahooks.svg?branch=master
+ [build-status-link]: https://travis-ci.org/grokify/ringcentral-permahooks
+ [goreport-svg]: https://goreportcard.com/badge/github.com/grokify/ringcentral-permahooks
+ [goreport-link]: https://goreportcard.com/report/github.com/grokify/ringcentral-permahooks
+ [docs-godoc-svg]: https://img.shields.io/badge/docs-godoc-blue.svg
+ [docs-godoc-link]: https://godoc.org/github.com/grokify/ringcentral-permahooks
+ [license-svg]: https://img.shields.io/badge/license-MIT-blue.svg
+ [license-link]: https://github.com/grokify/ringcentral-permahooks/blob/master/LICENSE.md
---
+++ server.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/grokify/gotilla/encoding/jsonutil"
+	"github.com/joho/godotenv"
+
+	rc "github.com/grokify/go-ringcentral/client"
+	rcu "github.com/grokify/go-ringcentral/clientutil"
+	omr "github.com/grokify/oauth2more/ringcentral"
+)
+
+const (
+	ValidationTokenHeader = "Validation-Token"
+	SMSEventFilter        = "/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS"
+)
+
+var (
+	OutboundWebhookUrl           = "" // Simple inbound webhook like Zapier or Chathooks
+	InboundWebhookUrl            = "" // Server URL the RingCentral API will send to
+	CurrentWebhookSubscriptionId = "" // Current SubscriptionID to renew
+	ExpiresIn                    = 60 * 60 * 24 * 7
+	RenewalThresholdTime         = 60 * 60 * 24
+	RenewalIntervalTime          = 60 * 60
+)
+
+// EventFilters determines the RingCentral events this service will subscribe to.
+// Threshold is the threshold time (in seconds) remaining before subscription expiration when server should start to send renewal reminder notifications. This time is approximate. It cannot be less than the interval of reminder job execution. It also cannot be greater than a half of this subscription TTL.
+// Interval is the interval (in seconds) between reminder notifications. This time is approximate. It cannot be less than the interval of reminder job execution. It also cannot be greater than a half of threshold value.
+
+var RenewalEventFilter = GetRenewalEventFilter("~", RenewalThresholdTime, RenewalIntervalTime)
+var EventFilters = []string{SMSEventFilter, RenewalEventFilter}
+
+func GetRenewalEventFilter(s string, threshold, interval int) string {
+	return fmt.Sprintf("/restapi/v1.0/subscription/%s?threshold=%d&interval=%d", s, threshold, interval)
+}
+
+func SetEventFilters() {
+	RenewalEventFilter = GetRenewalEventFilter("~", RenewalThresholdTime, RenewalIntervalTime)
+	EventFilters = []string{SMSEventFilter, RenewalEventFilter}
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	log.Info("Handling webhook...")
+	// Check to see if ValidationToken is present. If so, return
+	// it immediately.
+	validationToken := r.Header.Get(ValidationTokenHeader)
+	if len(validationToken) > 0 {
+		log.Printf("%s: %s", ValidationTokenHeader, validationToken)
+		w.Header().Set(ValidationTokenHeader, validationToken)
+		return
+	}
+
+	// Read the body to check if this is a renewal event
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf(string(body))
+
+	rcu := &rcu.Event{}
+	err = json.Unmarshal(body, rcu)
+	if err != nil {
+		log.Printf("JSON Unmarshal Error: %s", err.Error())
+		return
+	}
+
+	// If this is renewal event, renew the webhook and return.
+	if rcu.Event == RenewalEventFilter {
+		err := renewWebhook()
+		if err != nil {
+			log.Printf("Error reading body: %v", err)
+			http.Error(w, "can't read body", http.StatusBadRequest)
+		}
+		return
+	}
+
+	// Forward the body to the Webhook URL
+	resp, err := http.Post(
+		ForwardWebhookUrl,
+		"application/json",
+		bytes.NewBuffer(body))
+	if err != nil {
+		log.Printf("Downstream webhook error: %s", err.Error())
+		return
+	} else if resp.StatusCode >= 300 {
+		log.Printf("Downstream webhook error: %v", resp.StatusCode)
+		return
+	}
+}
+
+func createhookHandler(w http.ResponseWriter, r *http.Request) {
+	err := createWebhook()
+	if err != nil {
+		log.Printf(err.Error())
+	}
+}
+
+func renewhookHandler(w http.ResponseWriter, r *http.Request) {
+	err := renewWebhook()
+	if err != nil {
+		log.Printf(err.Error())
+	}
+}
+
+func createWebhook() error {
+	log.Info("Creating Hook...")
+	apiClient, err := newRingCentralClient()
+	if err != nil {
+		return err
+	}
+
+	req := rc.CreateSubscriptionRequest{
+		EventFilters: EventFilters,
+		DeliveryMode: &rc.NotificationDeliveryModeRequest{
+			TransportType: "WebHook",
+			Address:       ThisWebhookUrl,
+		},
+		ExpiresIn: int32(ExpiresIn),
+	}
+	log.Info(jsonutil.MustMarshalString(req, true))
+
+	return handleWebhookResponse(
+		apiClient.PushNotificationsApi.CreateSubscription(
+			context.Background(),
+			req,
+		),
+	)
+}
+
+func renewWebhook() error {
+	log.Debug("Renewing Hook Id %v ...", CurrentWebhookSubscriptionId)
+	apiClient, err := newRingCentralClient()
+	if err != nil {
+		log.Printf("RENEW NEW RC CLIENT ERROR: %v", err.Error())
+		return err
+	}
+
+	return handleWebhookResponse(
+		apiClient.PushNotificationsApi.RenewSubscription(
+			context.Background(),
+			CurrentWebhookSubscriptionId,
+		),
+	)
+}
+
+func handleWebhookResponse(info rc.SubscriptionInfo, resp *http.Response, err error) error {
+	if err != nil {
+		return fmt.Errorf("%v: %v", "API Response Err", err.Error())
+	} else if resp.StatusCode > 299 {
+		return fmt.Errorf("RingCentral Subscription API request failure status code: %v", resp.StatusCode)
+	}
+
+	CurrentWebhookSubscriptionId = info.Id
+	log.Info("Created/renewed Webhook with Id: %s", CurrentWebhookSubscriptionId)
+	return nil
+}
+
+func newRingCentralClient() (*rc.APIClient, error) {
+	return rcu.NewApiClient(
+		omr.ApplicationCredentials{
+			ServerURL:    os.Getenv("RINGCENTRAL_SERVER_URL"),
+			ClientID:     os.Getenv("RINGCENTRAL_CLIENT_ID"),
+			ClientSecret: os.Getenv("RINGCENTRAL_CLIENT_SECRET"),
+		},
+		omr.UserCredentials{
+			Username:  os.Getenv("RINGCENTRAL_USERNAME"),
+			Extension: os.Getenv("RINGCENTRAL_EXTENSION"),
+			Password:  os.Getenv("RINGCENTRAL_PASSWORD"),
+		},
+	)
+}
+
+func loadEnv() error {
+	envPaths := []string{}
+	if len(os.Getenv("ENV_PATH")) > 0 {
+		envPaths = append(envPaths, os.Getenv("ENV_PATH"))
+	}
+	return godotenv.Load(envPaths...)
+}
+
+func main() {
+	err := loadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InboundWebhookUrl = os.Getenv("PERMAHOOKS_INBOUND_WEBHOOK_URL")
+	OutboundWebhookUrl = os.Getenv("PERMAHOOKS_OUTBOUND_WEBHOOK_URL")
+
+	shortRenewal := true
+	if shortRenewal {
+		ExpiresIn = 180
+		RenewalThresholdTime = 80
+		RenewalIntervalTime = 30
+	}
+	SetEventFilters()
+
+	http.Handle("/webhook", http.HandlerFunc(webhookHandler))
+	http.Handle("/createhook", http.HandlerFunc(createhookHandler))
+	http.Handle("/renewhook", http.HandlerFunc(renewhookHandler))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
