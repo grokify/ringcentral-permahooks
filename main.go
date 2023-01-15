c517e25990b9d67fae570a2cd8fa81a1d4a1bad4
John Wang <johncwang@gmail.com>
2023-01-14 17:02:58 -0800

    dep: go mod: update

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/grokify/gohttp/httpsimple"
 	"github.com/grokify/mogo/encoding/jsonutil"
 	"github.com/grokify/mogo/fmt/fmtutil"
-	"github.com/grokify/mogo/net/httputilmore"
+	"github.com/grokify/mogo/net/http/httputilmore"
 	"github.com/grokify/mogo/net/urlutil"
 	"github.com/grokify/mogo/strconv/strconvutil"
 	"github.com/joho/godotenv"
